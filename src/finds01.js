68f53e67a86fb46543f1f92569691af0c00dbcb4
villanuevapinoantonio <avilpinb@g.educaand.es>
2021-09-30 10:17:20 +0200

    "update finds e inserts"

---
+++ src/finds01.js
@@ -0,0 +1,8 @@
+/*
+Buscamos todas aquellos documentos en los que el campo "edad" sea "19"
+*/
+db.col01.find({edad:19})
+
+/*
+{ "_id" : ObjectId("615570b06b370a6126a6dc5f"), "nombre" : "Ana", "edad" : 19 }
+*/
